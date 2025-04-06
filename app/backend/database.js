903d5a7e5005edf1e9ef5e98c2619068a62246f8
TylerBernardo <tylerjbernardo@gmail.com>
2025-04-05 18:23:31 -0700

    fixed crashes with "npm run dev"

--- app/backend/database.js
+++ app/backend/database.js
@@ -1,4 +1,4 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const {getApps, initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
 const serviceAccount = require('../../hackathon-455921-8d8b3ed87f1a.json');
@@ -7,32 +7,39 @@ import {User} from "./user"
 import {Class} from "./class"
 
 export class Database{
+    database = null
     db = null
 
-    constructor() {}
+    constructor(_db) {
+        this.db = _db
+    }
 
     static async getDatabase(){
-        if(Database.db == null){
+        if(getApps().length == 0){
             console.log("setting up database")
             initializeApp({
                 credential: cert(serviceAccount)
             });
-            Database.db = getFirestore();
         }
-        return new Database()
+        if(Database.database == null){
+            Database.database = new Database(getFirestore()) //getFirestore();
+        }
+        //console.log(Database.database.db)
+        return Database.database
     } 
 
     async addUser(data){
-        if(Database.db == undefined){
+        var database = await Database.getDatabase()
+        if(Database.datebase == undefined){
             throw "Database has not been loaded yet"
         }
-        var userID = await Database.db.collection("users").doc("nextID").get()
+        var userID = await database.db.collection("users").doc("nextID").get()
         var nextID = userID.data()
         userID = nextID.next
-        await Database.db.collection("users").doc("nextID").set({"next":userID + 1})
+        await database.db.collection("users").doc("nextID").set({"next":userID + 1})
 
         var user = new User(userID,data.username,data.name)
-        const docRef = Database.db.collection("users").doc(user.userID.toString())
+        const docRef = database.db.collection("users").doc(user.userID.toString())
         var toSave = {  
             "userID" : user.userID,
             "name" : user.name,
@@ -47,10 +54,11 @@ export class Database{
     }
 
     async getUser(userID){
-        if(Database.db == undefined){
+        var database = await Database.getDatabase()
+        if(Database.database == undefined){
             throw "Database has not been loaded yet"
         }
-        const docRef = Database.db.collection("users").doc(userID.toString())
+        const docRef = database.db.collection("users").doc(userID.toString())
         const match = await docRef.get()
         if(!match.exists){
             throw "user does not exist"
@@ -60,10 +68,11 @@ export class Database{
     }
 
     async getAllUsers(){
-        if(Database.db == undefined){
+        var database = await Database.getDatabase()
+        if(Database.database == undefined){
             throw "Database has not been loaded yet"
         }
-        const doc = await Database.db.collection("users").get()
+        const doc = await database.db.collection("users").get()
         var toReturn = []
         doc.forEach(user => {
             if(user.data().next == undefined){
@@ -87,16 +96,17 @@ data:
 */
 
     async addClass(data){
-        if(Database.db == undefined){
+        var database = await Database.getDatabase()
+        if(Database.database == undefined){
             throw "Database has not been loaded yet"
         }
 
-        const idRef = await Database.db.collection("classes").doc("nextID").get()
+        const idRef = await database.db.collection("classes").doc("nextID").get()
         var nextID = idRef.data()
         var classID = nextID.next
-        await Database.db.collection("classes").doc("nextID").set({"next":classID + 1})
+        await database.db.collection("classes").doc("nextID").set({"next":classID + 1})
 
-        const docRef = Database.db.collection("classes").doc(classID.toString())
+        const docRef = database.db.collection("classes").doc(classID.toString())
         data["classID"] = classID
 
         await docRef.set(data)
@@ -104,11 +114,12 @@ data:
 
 
     async getClass(classID){
-        if(Database.db == undefined){
+        var database = await Database.getDatabase()
+        if(Database.database == undefined){
             throw "Database has not been loaded yet"
         }
 
-        const docRef = Database.db.collection("classes").doc(classID.toString())
+        const docRef = database.db.collection("classes").doc(classID.toString())
         const match = await docRef.get()
         if(!match.exists){
             throw "class does not exist"
@@ -118,10 +129,11 @@ data:
     }
 
     async getClasses(){
-        if(Database.db == undefined){
+        var database = await Database.getDatabase()
+        if(Database.database == undefined){
             throw "Database has not been loaded yet"
         }
-        const doc = await Database.db.collection("classes").get()
+        const doc = await database.db.collection("classes").get()
         var toReturn = []
         doc.forEach(_class => {
             if(_class.data().next == undefined){
@@ -129,5 +141,9 @@ data:
             }
         })
         return toReturn
+    }
+
+    async addEvent(){
+
     }
 }
