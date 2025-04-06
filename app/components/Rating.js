9c1b9a9a0f381126fc1a625c79bab8ef3a9d9f33
maharl <maharl@oregonstate.edu>
2025-04-06 10:33:16 -0700

    Added Ability to view a instructor's profile

--- app/components/Rating.js
+++ app/components/Rating.js
@@ -20,9 +20,9 @@ export default function({rating}) {
     }
 
     return (
-        <div className="p-2 bg-black w-fit rounded-lg flex">
+        <div className="p-2 bg-gray-800 w-fit rounded-lg flex">
             {amts.map((val, idx) => {
-                return <svg key={idx} className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path><g className="" transform="translate(0,0)"><path d="M256 38.013c-22.458 0-66.472 110.3-84.64 123.502-18.17 13.2-136.674 20.975-143.614 42.334-6.94 21.358 84.362 97.303 91.302 118.662 6.94 21.36-22.286 136.465-4.116 149.665 18.17 13.2 118.61-50.164 141.068-50.164 22.458 0 122.9 63.365 141.068 50.164 18.17-13.2-11.056-128.306-4.116-149.665 6.94-21.36 98.242-97.304 91.302-118.663-6.94-21.36-125.444-29.134-143.613-42.335-18.168-13.2-62.182-123.502-84.64-123.502z" fill={color} fillOpacity={val} stroke="#ccc" strokeOpacity="1" strokeWidth="8"></path></g></svg>
+                return <svg key={idx} className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z" fill="#1e2939" fillOpacity="1"></path><g className="" transform="translate(0,0)"><path d="M256 38.013c-22.458 0-66.472 110.3-84.64 123.502-18.17 13.2-136.674 20.975-143.614 42.334-6.94 21.358 84.362 97.303 91.302 118.662 6.94 21.36-22.286 136.465-4.116 149.665 18.17 13.2 118.61-50.164 141.068-50.164 22.458 0 122.9 63.365 141.068 50.164 18.17-13.2-11.056-128.306-4.116-149.665 6.94-21.36 98.242-97.304 91.302-118.663-6.94-21.36-125.444-29.134-143.613-42.335-18.168-13.2-62.182-123.502-84.64-123.502z" fill={color} fillOpacity={val} stroke="#ccc" strokeOpacity="1" strokeWidth="8"></path></g></svg>
             })}
         </div>
     )
